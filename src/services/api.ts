85027a14ad40687241aed09a2c283fca682474f6
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-05-05 15:05:16 +0300

    RC_2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,27 +1,22 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { ImageData } from "../App.types";
 
 const ACCESSKEY = '5Z-_8iNGawk-enCI3-LHMdLN0jAr9dD1GQwkeaZ-dh8';
 
-
-
-//Створюємо свій варіант аксіо
+// Створюємо свій варіант axios
 const instance = axios.create({
   baseURL: "https://api.unsplash.com",
 });
 
-export const requestProductsByQuery: (query: string, per_page: number, page: number) => Promise<any> = async (
-    query = "",
-    per_page = 12,
-    page = 1
-  ) => {
-    const { data }:ImageData = await instance.get(
-      `/search/photos?client_id=${ACCESSKEY}&query=${query}&per_page=${per_page}&page=${page}`
-    );
-    
-    // console.log('data', data);
-    
+export const requestProductsByQuery = async (
+  query = "",
+  per_page = 12,
+  page = 1
+): Promise<ImageData> => {
+  const response: AxiosResponse<ImageData> = await instance.get(
+    `/search/photos?client_id=${ACCESSKEY}&query=${query}&per_page=${per_page}&page=${page}`
+  );
 
-    return data;
-  };
-  
+  // console.log('data', response.data);
+  return response.data;
+};
